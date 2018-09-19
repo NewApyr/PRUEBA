407d94c9e3a16b5ddc9d657b04291188fd00d3c0
Behemut <kevin_eliu@hotmail.es>
2018-09-18 19:33:26 -0600

    PRUEBA_PROGRA

---
+++ Calculadora_Matrices/Calculadora_Matrices/Form1.Designer.vb
@@ -0,0 +1,207 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Numeric1A = New System.Windows.Forms.NumericUpDown()
+        Me.Numeric2A = New System.Windows.Forms.NumericUpDown()
+        Me.Numeric1B = New System.Windows.Forms.NumericUpDown()
+        Me.Numeric2B = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.Numeric1A, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Numeric2A, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Numeric1B, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Numeric2B, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(28, 123)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(252, 212)
+        Me.ListBox1.TabIndex = 0
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 16
+        Me.ListBox2.Location = New System.Drawing.Point(356, 123)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(245, 212)
+        Me.ListBox2.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(122, 383)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(442, 383)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox3
+        '
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.ItemHeight = 16
+        Me.ListBox3.Location = New System.Drawing.Point(672, 51)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(254, 180)
+        Me.ListBox3.TabIndex = 4
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(772, 253)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Numeric1A
+        '
+        Me.Numeric1A.Location = New System.Drawing.Point(21, 61)
+        Me.Numeric1A.Name = "Numeric1A"
+        Me.Numeric1A.Size = New System.Drawing.Size(120, 22)
+        Me.Numeric1A.TabIndex = 6
+        '
+        'Numeric2A
+        '
+        Me.Numeric2A.Location = New System.Drawing.Point(160, 61)
+        Me.Numeric2A.Name = "Numeric2A"
+        Me.Numeric2A.Size = New System.Drawing.Size(120, 22)
+        Me.Numeric2A.TabIndex = 7
+        '
+        'Numeric1B
+        '
+        Me.Numeric1B.Location = New System.Drawing.Point(344, 61)
+        Me.Numeric1B.Name = "Numeric1B"
+        Me.Numeric1B.Size = New System.Drawing.Size(120, 22)
+        Me.Numeric1B.TabIndex = 8
+        '
+        'Numeric2B
+        '
+        Me.Numeric2B.Location = New System.Drawing.Point(503, 61)
+        Me.Numeric2B.Name = "Numeric2B"
+        Me.Numeric2B.Size = New System.Drawing.Size(120, 22)
+        Me.Numeric2B.TabIndex = 9
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 17)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Filas A"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(176, 31)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 17)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Columnas A"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(341, 31)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(50, 17)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Filas B"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(500, 31)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(83, 17)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Columnas B"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(959, 438)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Numeric2B)
+        Me.Controls.Add(Me.Numeric1B)
+        Me.Controls.Add(Me.Numeric2A)
+        Me.Controls.Add(Me.Numeric1A)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ListBox3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Numeric1A, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Numeric2A, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Numeric1B, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Numeric2B, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents ListBox3 As System.Windows.Forms.ListBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Numeric1A As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Numeric2A As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Numeric1B As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Numeric2B As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ Calculadora_Matrices/Calculadora_Matrices/Form1.vb
@@ -0,0 +1,87 @@
+﻿Public Class Form1
+    Dim x, y As Integer
+    Dim m, n As Integer
+    'Matriz A con x Filas   y Columnas 
+    'Matriz B m filas y n columunas
+    Dim MatrizA(5, 5) As Integer
+    Dim MatrizB(5, 5) As Integer
+    Dim MatrizC(5, 5) As Integer
+
+    
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        For i = 1 To x Step 1
+            For j = 1 To y Step 1
+                MatrizA(i, j) = InputBox("Introduzca los valores Matriz A ( " & x & " , " & y & " ) " & "   posicion   " & i & " , " & j)
+                ListBox1.Items.Add("Posiciones A: ( " & i & " , " & j & ") =  " & MatrizA(i, j))
+            Next
+        Next
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        For i = 1 To m Step 1
+            For j = 1 To n Step 1
+                MatrizB(i, j) = InputBox("Introduzca los valores Matriz B ( " & m & " , " & n & " )  " & "  posicion   " & i & " , " & j)
+                ListBox2.Items.Add("Posiciones B: ( " & i & " , " & j & ") =  " & MatrizB(i, j))
+            Next
+        Next
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        For i = 1 To x Step 1
+            For j = 1 To n Step 1
+                MatrizC(i, j) = 0
+            Next
+        Next
+
+        For i = 1 To x Step 1
+            For j = 1 To n Step 1
+                For z = 1 To m Step 1
+                    MatrizC(i, j) += MatrizA(i, z) * MatrizB(z, j)
+                Next
+            Next
+        Next
+
+        For i = 1 To x Step 1
+            For j = 1 To n Step 1
+                ListBox3.Items.Add(MatrizC(i, j) & "  ")
+            Next
+        Next
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Numeric1A.Minimum = 1
+        Numeric2A.Minimum = 1
+
+        Numeric1A.Maximum = 5
+        Numeric2A.Maximum = 5
+
+
+        Numeric1B.Minimum = 1
+        Numeric2B.Minimum = 1
+
+        Numeric1B.Maximum = 5
+        Numeric2B.Maximum = 5
+
+    End Sub
+
+    Private Sub Numeric1A_ValueChanged(sender As Object, e As EventArgs) Handles Numeric1A.ValueChanged
+        x = Numeric1A.Value
+    End Sub
+
+    Private Sub Numeric2A_ValueChanged(sender As Object, e As EventArgs) Handles Numeric2A.ValueChanged
+        y = Numeric2A.Value
+    End Sub
+
+    Private Sub Numeric1B_ValueChanged(sender As Object, e As EventArgs) Handles Numeric1B.ValueChanged
+        m = Numeric1B.Value
+
+    End Sub
+
+    Private Sub Numeric2B_ValueChanged(sender As Object, e As EventArgs) Handles Numeric2B.ValueChanged
+        n = Numeric2B.Value
+    End Sub
+End Class
